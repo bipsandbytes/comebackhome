e418d6842fad0585c40b0ca0acd2a4c04ce4560f
Bipin Suresh <bipin@coursera.org>
2015-03-21 11:13:10 -0700

    Simplify fetching results and use own server

--- jquery.404found.js
+++ jquery.404found.js
@@ -8,36 +8,19 @@
         });
         return $('#missing_people').each(function() {
             var template = $.templates(settings.item_template);
-            var country = "en_US";
             var el = this;
-            $.fn.found.fetch_feed(country, settings.max_results, function(data) {
-              template.link(el, data);
+            var country = "en_US";
+            var params = {
+                limit: settings.max_results,
+                format: 'json',
+            };
+            $.getJSON(settings.feed_url, params).done(function(data) {
+                var missing = _.shuffle(data.objects);
+                template.link(el, missing);
             });
         });
     };
 })(jQuery, window, document);
-
-$.fn.found.fetch_feed = function(country, max_results, success) {
-    var feed = "https://query.yahooapis.com/v1/public/yql?format=json&q=select%20*%20from%20xml%20where%20url%3D%22http%3A%2F%2Fwww.missingkids.com%2Fmissingkids%2Fservlet%2FXmlServlet%3Fact%3Drss%26" + "LanguageCountry%3D" + country + "%26orgPrefix%3DNCMC%26" + "%22";
-    $.get(feed, $.proxy(function(data) {
-        var missing = data.query.results.rss.channel.item;
-        missing = _.filter(missing, function(m) { return m.enclosure; });
-        missing = _.map(missing, function(m) {
-            m.title = m.title.replace(/^(.*?): /, "");
-            m.title = m.title.replace(/ \(.*?\)$/, "");
-            m.title = m.title.toLowerCase();
-            m.name = m.title;
-            m.img_url = m.enclosure.url.replace(/t.jpg/, ".jpg");
-            return m;
-        });
-
-        // randomize list each time
-        missing = _.shuffle(missing);
-        missing = missing.slice(0, max_results);
-
-        success(missing);
-    }, this));
-};
 
 $.fn.found.frame_template = "\
     <div id='person_found_frame'>\
@@ -55,8 +38,8 @@ $.fn.found.frame_template = "\
 
 $.fn.found.item_template = "\
     <li>\
-      <a href='{{:link}}' title='{{:name}}' target='_blank'>\
-          <img src='{{:img_url}}' alt='{{:name}}'>\
+      <a href='{{:url}}' title='{{:name}}' target='_blank'>\
+          <img src='{{:thumbnail_url}}' alt='{{:name}}'>\
       </a>\
     </li>\
 ";
@@ -65,5 +48,5 @@ $.fn.found.defaults = {
   max_results: 10,
   frame_template: $.fn.found.frame_template,
   item_template: $.fn.found.item_template,
-  fetch_feed: $.fn.found.fetch_feed
+  feed_url: "http://52.0.62.66:8000/api/v1/person/",
 };
