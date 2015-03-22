ec3e8bd42aac48ad4d7563c813cc49aeae3225e2
Yang Su <yang@coursera.org>
2015-03-21 19:27:36 -0700

    Setup build process
    - setup gulp with lint, stylus, and builds
    - setup package.json
    - setup bower.json
    - setup readme
    - updated license

---
+++ src/jquery.404found.js
@@ -0,0 +1,64 @@
+/*globals jQuery*/
+(function(factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD
+        define(['jquery'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        factory(require('jquery'));
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
+}(function($) {
+
+    $.fn.found = function(options) {
+        var settings = $.extend({}, $.fn.found.defaults, options);
+        this.append(settings.frame_template);
+        $("#person_found_frame").click(function(){
+            $("#person_found_panel").slideToggle();
+
+        });
+        return $('#missing_people').each(function() {
+            var template = $.templates(settings.item_template);
+            var el = this;
+            var country = "en_US";
+            var params = {
+                limit: settings.max_results,
+                format: 'json',
+            };
+            $.getJSON(settings.feed_url, params).done(function(data) {
+                var missing = _.shuffle(data.objects);
+                template.link(el, missing);
+            });
+        });
+    };
+    $.fn.found.frame_template = "\
+        <div id='person_found_frame'>\
+            <div id='person_found_title' class='pulsating'>404 PERSON NOT FOUND\
+              <div id='person_found_desc'>Help find missing people in your area</div>\
+            </div>\
+            <div id='person_found_panel'>\
+                <h1>Help find missing people in your area</h1>\
+                <ul class='polaroids '>\
+                    <div id='missing_people'></div>\
+                </ul>\
+            </div>\
+        </div>\
+    ";
+
+    $.fn.found.item_template = "\
+        <li>\
+          <a href='{{:url}}' title='{{:name}}' target='_blank'>\
+              <img src='{{:thumbnail_url}}' alt='{{:name}}'>\
+          </a>\
+        </li>\
+    ";
+
+    $.fn.found.defaults = {
+      max_results: 10,
+      frame_template: $.fn.found.frame_template,
+      item_template: $.fn.found.item_template,
+      feed_url: "http://52.0.62.66:8000/api/v1/person/",
+    };
+
+}));
