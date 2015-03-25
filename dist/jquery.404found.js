6483c68bc1ab59e94e30cf085693fc9bc659a332
Yang Su <yang@coursera.org>
2015-03-24 23:05:21 -0700

    Added bundle task that bundles together js, css, and template sources

--- dist/jquery.404found.js
+++ dist/jquery.404found.js
@@ -10,6 +10,7 @@
     factory(jQuery);
   }
 }(function($) {
+  var templates = {};
   /*globals $, templates*/
 var apiURL = 'http://52.0.62.66:8000/api/v1/person/';
 
@@ -44,14 +45,11 @@ $.found = function(target, options) {
 
 $.found.getData = getData;
 
-var templates = {};
-templates["body"] = function anonymous(data
-/**/) {
+templates["body"] = function anonymous(data) {
 var out='<div class="comebackhome-container"> <div class="comebackhome-header"> <div class="comebackhome-title">404 PERSON NOT FOUND</div> <div class="comebackhome-subheading">Help find missing people in your area</div> </div> <div class="comebackhome-panel"> <ul class="comebackhome-results"></ul> </div></div>';return out;
 };
-templates["items"] = function anonymous(data
-/**/) {
+templates["items"] = function anonymous(data) {
 var out='';var arr1=data;if(arr1){var item,i1=-1,l1=arr1.length-1;while(i1<l1){item=arr1[i1+=1];out+='<li class="comebackhome-person"> <a class="comebackhome-link" href=\''+( item.url )+'\' title=\''+( item.name )+'\' target=\'_blank\'> <div class="comebackhome-image" style="background-image:url('+( item.thumbnail_url )+');" alt=\''+( item.name )+'\'></div> <div class="comebackhome-name-container"> <span class="comebackhome-name">'+( item.name )+'</span> </div> </a></li>';} } return out;
-};return $.found;
-
+};
+  return $.found;
 }));
