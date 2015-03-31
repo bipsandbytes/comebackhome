b5e161074639ee9456c85f69fee3076fc239a6a8
Bipin Suresh <bipsandbytes@yahoo.com>
2015-03-30 20:55:05 -0700

    Always place banner over everything else

--- dist/jquery.404found.js
+++ dist/jquery.404found.js
@@ -11,19 +11,13 @@
   }
 }(function($) {
   var templates = {};
-  templates["body"] = function anonymous(data
-/**/) {
-var out='<div class="comebackhome-container"> <div class="comebackhome-header"> <div class="comebackhome-pulluptab">^ Help find missing people</div> <div class="comebackhome-title">404 Person Not Found</div> </div> <div class="comebackhome-panel"> <ul class="comebackhome-results"></ul> </div></div>';return out;
-};
-templates["items"] = function anonymous(data
-/**/) {
-var out='';var arr1=data;if(arr1){var item,i1=-1,l1=arr1.length-1;while(i1<l1){item=arr1[i1+=1];out+='<li> <a href="'+( item.url)+'" target=\'__blank\'> <div class="comebackhome-person-frame"> <div class="comebackhome-person-col"> <img class="comebackhome-person-picture" src="http://res.cloudinary.com/comebackhome/image/fetch/w_150,h_150,c_fill,f_auto,g_face:center,e_grayscale/'+( item.thumbnail_url )+'" alt="'+( item.name )+'"> </div> <div class="comebackhome-person-col"> <div class="comebackhome-person-name">'+( item.display_name )+'</div> <div class="comebackhome-person-location">'+( item.display_location )+'</div> <div class="comebackhome-person-extra"> Missing since '+( new Date(item.since).toDateString() )+'<br> Age now: '+( item.age_now )+'<br> </div> </div> </div> </a></li>';} } return out;
-};
-/*globals $, templates*/
+  /*globals $, templates*/
 var apiURL = 'http://52.0.62.66:8000/api/v1/person/';
 
 var defaults = {
-  limit: 10
+  limit: 10,
+  lat: 37.7577390,
+  lon: -122.4020760,
 };
 
 var getData = function(options) {
@@ -52,14 +46,23 @@ $.found = function(target, options) {
   var template = templates.items;
   // TODO: get a larger number of results, and then select a few
   // TODO: so that you get a different set each time
+  options.limit = 50;
   getData(options).done(function(data) {
     // shuffle the results around to randomize the results
-    var missing = shuffle(data.objects);
+    var missing = shuffle(data.objects).slice(0, 6);
     $results.html(template(missing));
   });
 };
 
 $.found.getData = getData;
 
+templates["body"] = function anonymous(data
+/**/) {
+var out='<div class="comebackhome-container"> <div class="comebackhome-header"> <div class="comebackhome-pulluptab">^ Help find missing people</div> <div class="comebackhome-title">404 Person Not Found</div> </div> <div class="comebackhome-panel"> <ul class="comebackhome-results"></ul> </div></div>';return out;
+};
+templates["items"] = function anonymous(data
+/**/) {
+var out='';var arr1=data;if(arr1){var item,i1=-1,l1=arr1.length-1;while(i1<l1){item=arr1[i1+=1];out+='<li> <a href="'+( item.url)+'" target=\'__blank\'> <div class="comebackhome-person-frame"> <div class="comebackhome-person-col"> <img class="comebackhome-person-picture" src="http://res.cloudinary.com/comebackhome/image/fetch/w_150,h_150,c_fill,f_auto,g_face:center,e_grayscale/'+( item.thumbnail_url )+'" alt="'+( item.name )+'"> </div> <div class="comebackhome-person-col"> <div class="comebackhome-person-name">'+( item.display_name )+'</div> <div class="comebackhome-person-location">'+( item.display_location )+'</div> <div class="comebackhome-person-extra"> Missing since '+( new Date(item.since).toDateString() )+'<br> Age now: '+( item.age_now )+'<br> </div> </div> </div> </a></li>';} } return out;
+};
   return $.found;
 }));
