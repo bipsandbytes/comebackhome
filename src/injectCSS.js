8f881b2e5226e155c0d3cdf44e4a8e2c31198d30
Bipin Suresh <bipin@coursera.org>
2015-04-12 13:20:15 -0700

    Center results in page

--- src/injectCSS.js
+++ src/injectCSS.js
@@ -1,8 +1,14 @@
+/*globals util*/
 // Shameless copied from https://github.com/substack/insert-css
 var elem = document.createElement('style');
 elem.setAttribute('type', 'text/css');
 
 var css = '<%= contents %>';
+
+// use window width to center results
+var width = Math.floor(util.documentWidth()/400)*400;
+css = css.replace('comebackhome-results{', 'comebackhome-results{width:' + width + 'px;');
+
 if ('textContent' in elem) {
   elem.textContent = css;
 } else {
