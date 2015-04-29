62997f80da4a19f1bd91e265f99c55b5a68a2791
Yang Su <yang@coursera.org>
2015-04-29 16:17:56 -0700

    Don't specify protocol in google analytics url

--- dist/zh_TW/comebackhome.js
+++ dist/zh_TW/comebackhome.js
@@ -169,7 +169,7 @@ util.ready = function(fn) {
 util.trackUsage = function() {
     var googleAnalyticsScript = document.createElement('script');
     googleAnalyticsScript.type = 'text/javascript';
-    googleAnalyticsScript.text = "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','http://www.google-analytics.com/analytics.js','ga'); ga('create', 'UA-61554464-1', 'auto'); ga('send', 'pageview');";
+    googleAnalyticsScript.text = "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga'); ga('create', 'UA-61554464-1', 'auto'); ga('send', 'pageview');";
     document.getElementsByTagName('head')[0].appendChild(googleAnalyticsScript);
 };
 
