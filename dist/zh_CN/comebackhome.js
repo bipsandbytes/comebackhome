354b6b878e01ea88773d43e9e4815a87e4d596c8
Yang Su <yang@coursera.org>
2015-04-28 17:25:45 -0700

    Fall back to sf coordinates if error

--- dist/zh_CN/comebackhome.js
+++ dist/zh_CN/comebackhome.js
@@ -78,16 +78,18 @@ util.getJSON = function(url, data) {
     }
   };
   request.send(data);
-  return {
+  var returnValue = {
     success: function(callback) {
       methods.success = callback;
-      return methods;
+      return returnValue;
     },
     error: function(callback) {
       methods.error = callback;
-      return methods;
+      return returnValue;
     }
   };
+
+  return returnValue;
 };
 
 // Based on http://stackoverflow.com/questions/6348494/addeventlistener-vs-onclick
@@ -204,17 +206,22 @@ util.getViewport = function() {
 
 /*globals util, templates*/
 
-var apiURL = 'http://comebackhome.org/api/v1/person/';
-var ipURL = 'https://freegeoip.net/json/';
+var API_URL = 'http://comebackhome.org/api/v1/person/';
+var IPLOOKUP_URL = 'https://freegeoip.net/json/';
+// default to San Francisco
+var DEFAULT_LOCATION = {
+  latitude: 37.7833,
+  longitude: -122.4167
+};
 
 var itemWidth = 310;
 var itemHeight = 180;
 
 var getData = function(options) {
-  return util.getJSON(apiURL, util.extend({}, options));
+  return util.getJSON(API_URL, util.extend({}, options));
 };
 
-var getUserLocation = util.getJSON.bind(util, ipURL);
+var getUserLocation = util.getJSON.bind(util, IPLOOKUP_URL);
 
 var called = false;
 var comebackhome = function($target, options) {
@@ -245,7 +252,7 @@ var comebackhome = function($target, options) {
   $results.style.width = containerWidth + 'px';
 
   var template = templates.items;
-  getUserLocation().success(function(location) {
+  var showResults = function(location) {
     options = util.extend({
       lat: Math.round(location.latitude),
       lon: Math.round(location.longitude)
@@ -259,7 +266,10 @@ var comebackhome = function($target, options) {
         util.toggleClass($title, 'comebackhome-title-throb');
       });
     });
-  });
+  };
+  getUserLocation()
+    .success(showResults)
+    .error(showResults.bind(null, DEFAULT_LOCATION));
 
   util.trackUsage();
 };
